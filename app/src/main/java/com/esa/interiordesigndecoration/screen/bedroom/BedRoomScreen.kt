3003316fd4870c9c3b5785b741e2f366490c415d
Kresa12 <aurakresa12@gmail.com>
2025-06-27 02:04:47 +0700

    bedroom screen create file only

---
+++ app/src/main/java/com/esa/interiordesigndecoration/screen/bedroom/BedRoomScreen.kt
@@ -0,0 +1,9 @@
+package com.esa.interiordesigndecoration.screen.bedroom
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BedRoomScreen(modifier: Modifier = Modifier) {
+    
+}
