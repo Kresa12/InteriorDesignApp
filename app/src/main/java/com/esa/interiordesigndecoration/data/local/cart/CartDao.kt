b5055dd140a1ab150cb21c806c20a818e6e542ab
Kresa12 <aurakresa12@gmail.com>
2025-07-23 17:47:35 +0700

    feat(cart): WIP make feature cart
    make Cart DAO

---
+++ app/src/main/java/com/esa/interiordesigndecoration/data/local/cart/CartDao.kt
@@ -0,0 +1,19 @@
+package com.esa.interiordesigndecoration.data.local.cart
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+interface CartDao {
+
+    @Query("SELECT * FROM cart")
+    fun getAllCart(): Flow<List<CartEntity?>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(cart : CartEntity)
+
+    @Delete
+    suspend fun delete(cart : CartEntity)
+}
